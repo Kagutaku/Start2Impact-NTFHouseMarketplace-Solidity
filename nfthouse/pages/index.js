f29b0904ec04e8af5d6cc1d11767b1912da50a50
Emanuele Bashuri <bashuri.emanuele1@gmail.com>
2023-11-11 00:49:28 +0100

    first commit

---
+++ nfthouse/pages/index.js
@@ -0,0 +1,13 @@
+const Page = () => {
+
+  return (
+    <>
+    <div className="centered-container">
+      <img className="homepage-logo" src="/icons/logo2.png" alt="NFTHome Marketplace Logo" />
+      <h1 className="homepage-description">NFTHome Marketplace: Your gateway to own your future house with one click thanks to the blockchain technology.</h1>
+    </div>
+    </>
+  )
+}
+  
+export default Page;
